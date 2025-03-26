d5a4915f0451104ef7ae54d1971b56513071358a
Dongja <129161266+jds79793@users.noreply.github.com>
2025-03-26 23:26:49 +0900

    fucntion

--- src/main/kotlin/chapter3/ExpressionBodyFunction.kt
+++ src/main/kotlin/chapter3/ExpressionBodyFunction.kt
@@ -8,4 +8,15 @@ class ExpressionBodyFunction {
     }
     /* 식이 본문인 함수 */
     fun circleArea2(radius: Double) = PI * radius * radius
+
+}
+
+fun restrictToRange(
+    from: Int = Int.MIN_VALUE,
+    to: Int = Int.MAX_VALUE,
+    value: Int
+): Int = Math.max(from, Math.min(to, value))
+
+fun main() {
+    println(restrictToRange(12, value = 1))
 }
